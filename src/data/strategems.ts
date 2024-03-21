d3c8469c7f5c2acdeed006f9a56ef1a244fdb9b0
Addison Dalton <zarch14@gmail.com>
2024-03-20 23:06:39 -0400

    chore: added more strategems

--- src/data/strategems.ts
+++ src/data/strategems.ts
@@ -25,6 +25,57 @@ const patioticAdminCenterStrategems: Strategem[] = [
       Input.Up,
       Input.Left
     ]
+  },
+  {
+    name: 'Expendable Anti-Tank',
+    category: 'Patriotic Administration Center',
+    type: 'weapon',
+    sequence: [Input.Down, Input.Down, Input.Left, Input.Up, Input.Right]
+  },
+  {
+    name: 'Recoilless Rifle',
+    category: 'Patriotic Administration Center',
+    type: 'weapon',
+    sequence: [Input.Down, Input.Left, Input.Right, Input.Right, Input.Left]
+  },
+  {
+    name: 'Flamethrower',
+    category: 'Patriotic Administration Center',
+    type: 'weapon',
+    sequence: [Input.Down, Input.Left, Input.Up, Input.Down, Input.Up]
+  },
+  {
+    name: 'Autocannon',
+    category: 'Patriotic Administration Center',
+    type: 'weapon',
+    sequence: [
+      Input.Down,
+      Input.Left,
+      Input.Down,
+      Input.Up,
+      Input.Up,
+      Input.Right
+    ]
+  },
+  {
+    name: 'Railgun',
+    category: 'Patriotic Administration Center',
+    type: 'weapon',
+    sequence: [
+      Input.Down,
+      Input.Right,
+      Input.Left,
+      Input.Down,
+      Input.Up,
+      Input.Left,
+      Input.Right
+    ]
+  },
+  {
+    name: 'Spear',
+    category: 'Patriotic Administration Center',
+    type: 'weapon',
+    sequence: [Input.Down, Input.Down, Input.Up, Input.Down, Input.Down]
   }
 ];
 
