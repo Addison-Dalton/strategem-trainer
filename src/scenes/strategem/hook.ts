63e0b22b6984884f63bbaced3afcf2d286746db3
Addison Dalton <zarch14@gmail.com>
2024-03-20 21:56:29 -0400

    feat: added some animations and incorrect input handling

--- src/scenes/strategem/hook.ts
+++ src/scenes/strategem/hook.ts
@@ -1,19 +1,34 @@
 import { useState, useEffect } from 'react';
 import { Input } from 'types/enums';
 
+export const TIMER_DURATION = 200;
+
 export const useInput = (inputs: Input[], removeStrategem: () => void) => {
   const [correctInputCount, setCorrectInputCount] = useState(0);
+  const [incorrectInput, setIncorrectInput] = useState(false);
 
   const handleCorrectInput = () => {
     setCorrectInputCount((prev) => prev + 1);
     if (correctInputCount + 1 === inputs.length) {
       removeStrategem();
-      setCorrectInputCount(0);
+      resetInputCount();
     }
+  };
+
+  const resetInputCount = () => {
+    setCorrectInputCount(0);
+  };
+
+  const handleIncorrectInput = () => {
+    resetInputCount();
+    setIncorrectInput(true);
   };
 
   // keyboard input listerns for the arrow keys that correspond to the strategem inputs
   const handleKeyDown = (e: KeyboardEvent) => {
+    const key = e.key;
+    // only listen for arrow keys
+    if (!key.includes('Arrow')) return;
     if (e.key === 'ArrowUp' && inputs[correctInputCount] === Input.Up) {
       handleCorrectInput();
     } else if (
@@ -31,6 +46,8 @@ export const useInput = (inputs: Input[], removeStrategem: () => void) => {
       inputs[correctInputCount] === Input.Right
     ) {
       handleCorrectInput();
+    } else {
+      handleIncorrectInput();
     }
   };
 
@@ -41,7 +58,29 @@ export const useInput = (inputs: Input[], removeStrategem: () => void) => {
     };
   });
 
+  useEffect(() => {
+    if (incorrectInput) {
+      const timer = setTimeout(() => {
+        setIncorrectInput(false);
+      }, TIMER_DURATION);
+
+      return () => clearTimeout(timer);
+    }
+  }, [incorrectInput]);
+
   return {
+    incorrectInput,
     correctInputCount
+  };
+};
+
+export const getClassName = (
+  correctInputCount: number,
+  index: number,
+  incorrectInput: boolean
+) => {
+  if (incorrectInput) {
+    return 'incorrect-input';
   }
+  return correctInputCount > index ? 'correct-input' : '';
 };
--- src/scenes/strategem/input.tsx
+++ src/scenes/strategem/input.tsx
@@ -6,15 +6,50 @@ import {
   ImArrowLeft,
   ImArrowRight
 } from 'react-icons/im';
+import styled from '@emotion/styled';
+import { keyframes } from '@chakra-ui/react';
 import { Input } from 'types/enums';
 
-import { useInput } from './hook';
+import { useInput, getClassName, TIMER_DURATION } from './hook';
 
 type Props = {
   inputs: Input[];
   showHint?: boolean; // show inputs prior to being pressed
   removeStrategem: () => void;
 };
+
+const scaleUp = keyframes`
+  0% { transform: scale(1); }
+  50% { transform: scale(1.2); }
+  100% { transform: scale(1); }
+`;
+
+const SHAKE_LENGTH = 5;
+const shake = keyframes`
+  0% { transform: translate(0, 0); }
+  20% { transform: translate(-${SHAKE_LENGTH}px, 0); }
+  40% { transform: translate(${SHAKE_LENGTH}px, 0); }
+  60% { transform: translate(-${SHAKE_LENGTH}px, 0); }
+  80% { transform: translate(${SHAKE_LENGTH}px, 0); }
+  100% { transform: translate(0, 0); }
+`;
+
+const StyledIconBox = styled(Box)`
+  display: inline-block;
+  margin: 0 0.5rem;
+  color: white;
+  transition: all ${TIMER_DURATION}ms ease-in;
+
+  &.incorrect-input {
+    color: red;
+    animation: ${shake} ${TIMER_DURATION}ms ease-in-out;
+  }
+
+  &.correct-input {
+    color: var(--chakra-colors-hdYellow-500);
+    animation: ${scaleUp} ${TIMER_DURATION}ms ease-in-out;
+  }
+`;
 
 const inputIcons = {
   [Input.Up]: ImArrowUp,
@@ -24,20 +59,21 @@ const inputIcons = {
 };
 
 const StrategemInput = ({ inputs, removeStrategem }: Props) => {
-  const { correctInputCount } = useInput(inputs, removeStrategem);
+  const { correctInputCount, incorrectInput } = useInput(
+    inputs,
+    removeStrategem
+  );
   if (!inputs || inputs.length === 0) return null;
-
   return (
     <Box>
       {inputs.map((input, index) => (
-        <Box
+        <StyledIconBox
           key={`strategem-input-${index}`}
-          display="inline-block"
-          mx={1}
+          className={getClassName(correctInputCount, index, incorrectInput)}
           color={correctInputCount > index ? 'hdYellow.500' : 'white'}
         >
           <Icon boxSize="60px" as={inputIcons[input]} />
-        </Box>
+        </StyledIconBox>
       ))}
     </Box>
   );
